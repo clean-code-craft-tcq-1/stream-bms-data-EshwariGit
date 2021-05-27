d8f8afefb7a991bec9ba09198d74ace2e5da98b7
Eshwari Ramakrishna <77387810+EshwariGit@users.noreply.github.com>
2021-05-28 00:36:23 +0530

    Update Sender_stream.cpp

--- Sender_stream.cpp
+++ Sender_stream.cpp
@@ -2,7 +2,7 @@
 #define CATCH_CONFIG_MAIN 
 
 #define "test/Sender_stream.hpp"
-//#define "Sender/Sender_Stream.h"
+#define "Sender/Sender_Stream.h"
 
 TEST_CASE("Testcase to validate postive range value printed in console") 
 {
@@ -14,7 +14,7 @@ TEST_CASE("Testcase to validate negative range value printed in console")
  // REQUIRE(tobedefined(3,4,4)== 1);
 }
 
-TEST_CASE("Testcase to validate verify false postive range value printed in console"") 
+TEST_CASE("Testcase to validate verify false postive range value printed in console") 
 {
  // REQUIRE(tobedefined(3,4,4)== 1);
 }
