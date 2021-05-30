9f14eb673ec27d93c7d0d23b05dacc52e676d417
Eshwari Ramakrishna <77387810+EshwariGit@users.noreply.github.com>
2021-05-30 17:17:37 +0530

    Update Sender_stream.cpp

--- Sender_stream.cpp
+++ Sender_stream.cpp
@@ -1,7 +1,7 @@
 // Test driven Development. -> Define the testcase before implementation.
 #define CATCH_CONFIG_MAIN 
 
-#define "test/Sender_stream.hpp"
+#define "testsender/catch.hpp"
 #define "Sender/Sender_Stream.h"
 
 TEST_CASE("Testcase to validate postive range value printed in console") 
