389b0ffe3b966bf01ad3a0dce9fb9211961cb452
Eshwari Ramakrishna <77387810+EshwariGit@users.noreply.github.com>
2021-05-31 10:35:07 +0530

    Update Sender_Stream.h

--- Sender/Sender_Stream.h
+++ Sender/Sender_Stream.h
@@ -1,1 +1,55 @@
+/*
+ **********************************************************************************************************************
+ * Includes and Definition Section
+ **********************************************************************************************************************
+*/
 
+#define BMS_DATA 50
+
+/*
+ **********************************************************************************************************************
+ * TypeDef handling Section
+ **********************************************************************************************************************
+*/
+
+
+typedef struct {
+float min;
+float max;
+}bms_data;
+
+typedef enum
+{
+	FAIL,
+	PASS
+}Bms_result;
+
+typedef enum{
+	printtoconsole
+}BMS_outputtype;
+
+typedef enum{
+	FileInputType
+
+}BMS_inputtype;
+
+/*
+ **********************************************************************************************************************
+ * TypeDef handling Section
+ **********************************************************************************************************************
+*/
+
+Bms_result dataread(float Temperature[],float StateOfCharge[]);
+Bms_result outputtoconsole(float Temperature[],float StateOfCharge[]);
+Bms_result inputvalue(BMS_inputtype inputvalue);
+Bms_result outputvalue(BMS_outputtype outputvalue);
+
+
+/*
+ **********************************************************************************************************************
+ *Extern Declarations
+ **********************************************************************************************************************
+*/
+
+extern float Temperature[BMS_DATA];
+extern float StateOfCharge[BMS_DATA];
