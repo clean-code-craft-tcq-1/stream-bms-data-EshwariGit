23b12126cc0f604a587516e08c417212f9e86cbe
Eshwari Ramakrishna <77387810+EshwariGit@users.noreply.github.com>
2021-05-31 10:34:46 +0530

    Update Sender_Stream.c

--- Sender/Sender_Stream.c
+++ Sender/Sender_Stream.c
@@ -1,2 +1,69 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 
 #include "Sender_Stream.h"
+
+float Temperature[BMS_DATA] = {};
+float StateOfCharge[BMS_DATA] = {};
+int filevalue = 0;
+
+Bms_result (*BMSDataRead[])(float Temperature[],float StateOfCharge[])={dataread};
+Bms_result(*BMSoutput[])(float Temperature[],float StateOfCharge[])={printtoconsole};
+
+Bms_result dataread(float Temperature[],float StateOfCharge[])
+{
+
+    float TemperatureTemp,StateOfChargeVal;
+
+    Bms_result Status= FAIL;
+    int filevalue = 0;
+    FILE * file=fopen( "Sender_Text.txt" , "r");
+
+    if (file) {
+        for(int iterate_counter=0;fscanf(file, "%f\t\t%f\n", &TemperatureTemp,&StateOfChargeVal) ;iterate_counter++)
+        {
+            filevalue=filevalue+1;
+            Temperature[iterate_counter]=TemperatureTemp;
+            StateOfCharge[iterate_counter]=StateOfChargeVal;
+        }
+
+
+        Status= PASS;
+    }
+    fclose(file);
+    return Status;
+
+}
+
+
+Bms_result inputvalue(BMS_inputtype inputvalue)
+{
+
+    Bms_result FileReadSuccess = FAIL;
+    FileReadSuccess=(*BMSDataRead[inputvalue])(Temperature,StateOfChargeVal);
+    return FileReadSuccess;
+
+
+}
+
+Bms_result outputvalue(BMS_outputtype outputvalue)
+{
+
+    Bms_result Status = FAIL;
+    Status=(*BMSoutput[outputvalue])(Temperature,StateOfChargeVal, filevalue);
+    return Status;
+
+}
+
+Bms_result outputtoconsole(float Temperature[],float StateOfCharge[])
+{
+    for(int i=0;i<20;i++)
+    {
+        printf("Temp value is %f and StateOfCharge value is %f\n",Temperature[i],StateOfCharge[i]);
+    }
+    return PASS;
+
+
+}
+
